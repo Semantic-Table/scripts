414c180940bdba371e1e3215fcc8b5ee3cc660a2
marvin.le-peillet <marvin.le-peillet@mazedia.fr>
2023-05-01 23:42:53 +0200

    refacto

--- new-project.sh
+++ new-project.sh
@@ -1,46 +1,51 @@
 #!/usr/bin/env bash
-
-# -----------------------------------------------------------------
 # this script is done to facilitate the creation of a new project from a template, 
 # it create the project and if provide do the first push for you
-# -----------------------------------------------------------------
 
-function HELP() {
-    echo ""
-    echo "projectfromteplate template_adress project_name [repo_to_push] [commit_message]"
-    echo ""
+template_adress=${1}
+project_name=${2}
+repo_to_push=${3}
+commit_message=${4}
+
+help() {
+    if [[ ${#} == 1 && ${1} == "help" ]]; then
+        echo "projectfromteplate template_adress project_name [repo_to_push] [commit_message]"
+        exit
+    fi
 }
 
-template_adress=$1
-project_name=$2
-repo_to_push=$3
-commit_message=$4
-
-if [[ $# == 1 && $1 == "help" ]]; then
-    HELP
-    exit
-fi
-
-git clone $template_adress $project_name
-cd $project_name
-rm -rf .git
-npm i
-
-if [[ $? -eq 0 ]]; then 
-    code .
-    if ! [[ -z $repo_to_push ]]; then
-        git init
-        git add .
-
-        if ! [[ -z $commit_message ]]; then
-            git commit -m ":tada: $commit_message"    
-        else
-            git commit -m ":tada: begin project"
-        fi
-        
-        git branch -M main
-        git remote add origin $repo_to_push
-        git push -u origin main
+push_repository() {
+    git init
+    git add .
+    if ! [[ -z ${commit_message} ]]; then
+        git commit -m ":tada: ${commit_message}"    
+    else
+        git commit -m ":tada: begin project"
     fi
-fi
+    git branch -M main
+    git remote add origin $repo_to_push
+    git push -u origin main
+}
+
+clone_repo_to_blank() {
+    git clone ${template_adress} ${project_name}
+    cd ${project_name}
+    rm -rf .git
+    npm i
+}
+
+main() {
+    help
+    clone_repo_to_blank
+    if [[ $? -eq 0 ]]; then 
+        code .
+        if ! [[ -z ${repo_to_push} ]]; then
+        push_repository
+        fi
+    fi
+}
+
+main
+
+
 
